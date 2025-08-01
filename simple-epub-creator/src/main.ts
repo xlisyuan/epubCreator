d37a840717c376e60e8b0ce6e4607cec8c614e8f
xlisyuan <xlisyuan@gmail.com>
2025-08-02 02:32:17 +0800

    install Element Plus icon

--- simple-epub-creator/src/main.ts
+++ simple-epub-creator/src/main.ts
@@ -1,4 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
-createApp(App).mount('#app')
+// 引入 Element Plus 的所有圖示
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
+const app = createApp(App)
+
+// 將 Element Plus 註冊到 Vue 應用程式中
+app.use(ElementPlus)
+
+// 註冊所有 Element Plus 的圖示
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+
+app.mount('#app')
